debde2996ba86d90188b36a472a929d9a552fe52
DecentMakeover <Ryanloaded@live.com>
2024-07-21 16:28:29 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from langchain_openai import ChatOpenAI
 st.title('🦜🔗 Replies in Sam Altam Style')# 
 
 def generate_response(input_text):
-    llm = ChatOpenAI(model="ft:gpt-3.5-turbo-1106:yethi-consulting-pvt-ltd:1:9nMccwQZ", temperature=1,openai_api_key)
+    llm = ChatOpenAI(model="ft:gpt-3.5-turbo-1106:yethi-consulting-pvt-ltd:1:9nMccwQZ", temperature=1,openai_api_key=openai_api_key)
     # llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
     response = llm(input_text).content
     st.info(response)
